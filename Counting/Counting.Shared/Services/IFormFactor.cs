78b351f2162f20d9a688ab2630f0263a7ca5a182
nie.chao <nie.chao@advantech.com.cn>
2024-11-22 17:35:53 +0800

    init

---
+++ Counting/Counting.Shared/Services/IFormFactor.cs
@@ -0,0 +1,8 @@
+namespace Counting.Shared.Services
+{
+    public interface IFormFactor
+    {
+        public string GetFormFactor();
+        public string GetPlatform();
+    }
+}
---
+++ Counting/Counting/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Counting
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
