c299a40d44dcb723b77ba2ab2dfb2e3dab9a56b6
nie.chao <nie.chao@advantech.com.cn>
2024-11-25 18:07:19 +0800

    aics icon

---
+++ Counting/Counting.Shared/Components/AicsButton.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Counting.Shared.Components;
+
+public partial class AicsButton : ComponentBase
+{
+    
+}
+
+public enum ButtonSize
+{
+    Small = 12,
+    Medium = 14,
+    Large = 16
+}
+
+public enum ButtonType
+{
+    Primary = 0,
+    Secondary = 1,
+    OnStrong = 2,
+    Successful = 3,
+    Error = 4,
+    Warning = 5
+}
+
+public enum ButtonMode
+{
+    Filled = 0,
+    Outlined = 1,
+    Text = 2
+}
